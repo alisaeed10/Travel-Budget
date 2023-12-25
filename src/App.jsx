2c3a874a7a6404bc88fe300c5d961b32ad3de12b
ali <4lisaeed96@gmail.com>
2023-12-24 21:59:51 -0600

    changed small thing

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import './App.css'
 
 function App() {
  return (
-   <div className="Apps">
+   <div className="App">
      <BrowserRouter>
        <Routes>
          <Route path="/" element={<Welcome/>}/>
