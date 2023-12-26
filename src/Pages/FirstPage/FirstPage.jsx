3bd4c561bb9160d6e156fdde2630d4410814bffa
ali <4lisaeed96@gmail.com>
2023-12-25 23:42:23 -0600

    cleaned cached

---
+++ src/Pages/FirstPage/FirstPage.jsx
@@ -0,0 +1,9 @@
+
+export function Welcome() {
+    return (
+        <div>
+            <h1>Welcome to the React Router Tutorial</h1>
+            <small>Main Page</small>
+        </div>
+    );
+}
---
+++ src/Pages/SignIn/OAuthPage.jsx
@@ -0,0 +1,6 @@
+export function SignIn() {
+    return (<div>
+        <h1>OAuth</h1>
+        <small>OAuth Page</small>
+        </div>);
+}
