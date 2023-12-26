55ffbd52df21293b981d762b164d23c1a02a84ae
ali <4lisaeed96@gmail.com>
2023-12-25 23:42:11 -0600

    updated files

--- src/Pages/FirstPage/FirstPage.jsx
+++
@@ -1,9 +0,0 @@
-
-export function Welcome() {
-    return (
-        <div>
-            <h1>Welcome to the React Router Tutorial</h1>
-            <small>Main Page</small>
-        </div>
-    );
-}
--- src/Pages/SignIn/OAuthPage.jsx
+++
@@ -1,6 +0,0 @@
-export function SignIn() {
-    return (<div>
-        <h1>OAuth</h1>
-        <small>OAuth Page</small>
-        </div>);
-}
