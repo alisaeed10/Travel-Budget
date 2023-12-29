b8ddc4363cc36d1754399649588a02616c252839
MustafaRush731 <rushmus226@gmail.com>
2023-12-28 22:04:32 -0600

    firstChange

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Welcome } from './Pages/FirstPage/FirstPage'
 import {SignIn} from './Pages/SignIn/OAuthPage'
 
-
 import './App.css'
 
 
 function App() {
  return (
-   <div className="Apps">
+   <div className="App">
      <BrowserRouter>
        <Routes>
          <Route path="/" element={<Welcome/>}/>
-         <Route path="/signin" element={<SignIn/>}/>
+         {/* <Route path="/signin" element={<SignIn/>}/> */}
+         {/* <Route path="/" element={<Info/>}/> */}
        </Routes>
      </BrowserRouter>
    </div>
--- src/Pages/FirstPage/FirstPage.jsx
+++ src/Pages/FirstPage/FirstPage.jsx
@@ -1,9 +1,16 @@
+import videoBackground from '../../assets/video (1080p) (1).mp4'
+import './FirstPage.css'
 
 export function Welcome() {
     return (
-        <div>
-            <h1>Welcome to the React Router Tutorial</h1>
-            <small>Main Page</small>
+        <div className='main'>
+            <div className="overlay"></div>
+            <video src={videoBackground} autoPlay loop muted />
+            <div className='content'>
+                <h1>Adventures Await</h1>
+                <button className='btn'>Start Here</button>
+            </div>
         </div>
     );
 }
+
