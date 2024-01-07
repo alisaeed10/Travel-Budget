c48199793a253a6f6bdfa7f548ef33df3cfe1a9e
ali <4lisaeed96@gmail.com>
2024-01-06 22:21:40 -0600

    working on nav bar

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,9 +1,16 @@
+import { Link } from 'react-router-dom';
 import '../Pages/FirstPage/FirstPage.css';
 
 export function Nav(){
     return(
         <div className='NavBar'>
-            <p>Nav</p>
+            <div className="logo">
+                <img className='navbar-logo' src='../../../public/logo2.png' alt='logo'/>
+                <p className='navbar-tag'>Travel Budget</p>
+            </div>
+            <div className="nav-links">
+                <a href='/signin'>Sign In</a>
+            </div>
         </div>
     );
 }
--- src/Pages/FirstPage/FirstPage.jsx
+++ src/Pages/FirstPage/FirstPage.jsx
@@ -9,8 +9,6 @@ export function Welcome() {
         <div className='Welcome'>
             <div className="Welcome-header">
             <p className="Welcome-title">Travel to a World of Dreams</p>
-            
-            <p className="Welcome-bio">wanna travel but rather not spend a lot. Set a budget and We'll guarantee a fun trip under that budget.  </p>
            </div>
            <div className="Welcome-content">
                 <SlideShow />
