f379a1cfcbd4636c16247204a0fb66b79f425ce0
ali <4lisaeed96@gmail.com>
2024-07-01 13:50:40 -0500

    finished date set up

--- src/Components/infoSlides/NumDaysSlide.jsx
+++ src/Components/infoSlides/NumDaysSlide.jsx
@@ -1,7 +1,43 @@
-export function NumDaysSlide(){
-    return (
-        <div>
-            num of days
-        </div>
-    )   
+import Calendar from "react-calendar";
+import { useState } from "react";
+import '../../Pages/Info/InformationPage.css'
+
+
+export function NumDaysSlide() {
+  const [rangeDate, setRangeDate] = useState([new Date(), new Date()]);
+  const [startDate,setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  
+  const onChangeDate = (date) => {
+    setRangeDate(date);
+    // Array.isArray(date) will check if date is an array or not
+    if (Array.isArray(date) && date.length === 2) {
+      // date[0] instanceof Date will vaildate if date[0] is a date object to use the function toLocaleDateString()
+      setStartDate(date[0] instanceof Date ? date[0].toLocaleDateString() : "");
+      setEndDate(date[1] instanceof Date ? date[1].toLocaleDateString() : "");
+    }else{
+      setStartDate("");
+      setEndDate("");}
+    console.log(startDate);
+  };
+
+  return (
+    <div className="calendar">
+      <div className="startDate">
+        {startDate === "" ? "select a start date" : `start date : ${startDate} -`} {endDate === "" ? "" : `end date : ${endDate}`}
+      </div>
+
+      <Calendar
+        className="calendar-set"
+        locale="custom"
+        allowPartialRange={false}
+        selectRange={true}
+        returnValue="range"
+        onChange={onChangeDate}
+        minDate={new Date()}
+        showNeighboringMonth={false}
+        formatMonthYear={(locale, date) => date.toLocaleString(locale, { month: 'short', year: 'numeric' })}
+      />
+    </div>
+  );
 }
--- src/Components/infoSlides/NumPeopleSlides.jsx
+++ src/Components/infoSlides/NumPeopleSlides.jsx
@@ -1,7 +1,15 @@
-export function NumPeopleSlides(){
+import { useState } from 'react';
+
+export function NumPeopleSlides() {
+    const [numPeople,setNumPeople] = useState(1);
     return (
-        <>
-            people
-        </>
+        <div className="people">
+            <button className="people-button">1</button>
+            <button className="people-button">2</button>
+            <button className="people-button">3</button>
+            <button className="people-button">4</button>
+            <button className="people-button">5</button>
+            <button className="people-button">6</button>
+        </div>
     );
 }
